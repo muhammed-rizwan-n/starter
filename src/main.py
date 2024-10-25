d5cf5d83303dd69a24f8105309867fd1c2ded7dc
muhammed-rizwan-n <148784103+muhammed-rizwan-n@users.noreply.github.com>
2024-10-25 23:09:13 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from appwrite.client import Client
 from appwrite.services.users import Users
+from appwrite.services.databases import Databases
 from appwrite.exception import AppwriteException
+from appwrite.query import Query
 import os
 
 # This Appwrite function will be executed every time your function is triggered
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
     client = (
         Client()
         .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
@@ -16,32 +16,31 @@ def main(context):
     users = Users(client)
     databases = Databases(client)
 
-    result = databases.create_document(
-        database_id = '<DATABASE_ID>',
-        collection_id = '<COLLECTION_ID>',
-        document_id = '<DOCUMENT_ID>',
-        data = {},
-        )
-    
+    # Define your database ID and collection ID
+    database_id = os.environ["DATABASE_ID"]
+    collection_id = os.environ["COLLECTION_ID"]
+
+    # Extract the 'name' parameter from the URL if present
+    name = context.req.query.get("name")
+
     try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
+        if name:
+            # Query the database for a document with the matching name
+            response = databases.list_documents(
+                database_id,
+                collection_id,
+                [Query.equal("name", name)]
+            )
+
+            # Check if any documents match the query
+            if response['total'] > 0:
+                # Return the data of the first matching user
+                return context.res.json(response['documents'][0])
+            else:
+                return context.res.json({"error": "User not found"}, status=404)
+        else:
+            return context.res.json({"error": "Name parameter is missing"}, status=400)
+
     except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
-
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+        context.error("Error accessing database: " + repr(err))
+        return context.res.json({"error": "Failed to fetch user data"}, status=500)
